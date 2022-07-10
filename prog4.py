88abdc44b5d406bd9c4db1c5f9eecbf031b8413e
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-10 15:37:05 +0530

    Add files via upload
    calculate the average and total percentage

---
+++ prog4.py
@@ -0,0 +1,12 @@
+#calculate the average and percentage of 5 subjects
+sub1=float(input("enter the sub1 marks:"))
+sub2=float(input("enter the sub2 marks:"))
+sub3=float(input("enter the sub3 marks:"))
+sub4=float(input("enter the sub4 marks:"))
+sub5=float(input("enter the sub5 marks:"))
+totalmarks=500
+sum_marks=sub1+sub2+sub3+sub4+sub5
+average=sum_marks/5
+percentage=(sum_marks/500)*100
+print("average of subjects is:",average)
+print("percentage is :",percentage)
