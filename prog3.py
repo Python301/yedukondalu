a2064db6c78e8f20b25d93a107271a829a316337
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-10 15:18:26 +0530

    Add files via upload
    Arthematic operations

---
+++ prog3.py
@@ -0,0 +1,17 @@
+#WAPP to perform Arthematic operations
+num1=15
+num2=2
+addition=num1+num2
+substraction=num1-num2
+multiplication=num1*num2
+floatdivition=num1/num2
+intdivition=num1//num2
+modular=num1%num2
+power=num1**num2
+print("addition is ",addition)
+print("substraction is ",substraction)
+print("multiplication is ",multiplication)
+print("floatdivition is ",floatdivition)
+print("intdivition is ",intdivition)
+print("modular is ",modular)
+print("power is ",power)
