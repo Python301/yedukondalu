7e3ee34e99fc88955311075ffdb10d31ee715f4b
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-10 14:53:27 +0530

    Add files via upload
    WAPP print the address from user

---
+++ prog1.py
@@ -0,0 +1,10 @@
+#WAPP to print your address and input is ask from user:
+Name=input("enter your name:")
+#Address
+Hno=input("enter your house number:")
+Area=input("enter your area:")
+Village=input("enter your village:")
+Pin=int(input("enter your postalcode:"))
+State=input("enter your state:")
+print(Name)
+print("Address:",Hno,Area,Village,Pin,State)
