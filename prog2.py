bedc27f9cff9de3279ea6e89668b88cacc3d3e8f
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-10 15:05:17 +0530

    Add files via upload
    Student Details

---
+++ prog2.py
@@ -0,0 +1,8 @@
+#student details:
+stud_name=input("enter student name:")
+regno=int(input("enter student regno:"))
+course=input("enter your course:")
+semister=input("enter your semister:")
+percentage=float(input("enter your percentage:"))
+rank=int(input("enter your rank:"))
+print("student details is :",stud_name,regno,course,semister,percentage,rank)
