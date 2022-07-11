170c7b252670df2b21d2cfe39ebb8c43a799ea50
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-11 10:56:54 +0530

    Add files via upload
    To check the number is positive or negative

---
+++ prog6.py
@@ -0,0 +1,8 @@
+#check if the number is +ve or -ve
+num=int(input("enter the number:"))
+if(num>0):
+    print("the given number {} is positive number".format(num))
+if(num<0):
+    print("the given number {} is negative number".format(num))
+else:
+    print("program completed successfully")
