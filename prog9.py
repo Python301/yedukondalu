4382e90f86da965b939fc81cb730d08829db672c
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-11 11:34:56 +0530

    Add files via upload
    converts to centimeters

---
+++ prog9.py
@@ -0,0 +1,5 @@
+#WAPP to converts the foot into inches and also converts into centimeters
+feet=int(input("enter the feets:"))
+inch=int(input("enter the inches:"))
+centimeters=(feet*12*2.54)+(inch*2.54)
+print("converts the feet and inches into centimeter is:",centimeters)
