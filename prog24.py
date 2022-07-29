6067cb5456a558af828df8524d46f51377fc6549
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-29 19:43:32 +0530

    Add files via upload
    multiplication table

---
+++ prog24.py
@@ -0,0 +1,11 @@
+#multiple table
+n=int(input("enter the number:"))
+for i in range(1,11):
+    print("{}x{}={}".format(n,i,n*i))
+
+
+
+
+for i in range(1,n+1):
+    print(i*1,i*2,i*3,i*4,i*5,i*6,i*7,i*8,i*9,i*10)
+    print("*"*50)
