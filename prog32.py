707a939d2f02bd3a409109f03b00a22604065f30
Python301 <108857090+Python301@users.noreply.github.com>
2022-07-31 08:24:20 +0530

    Add files via upload
    WAPP to calculate the average of the marks(points)

---
+++ prog32.py
@@ -0,0 +1,7 @@
+#WAPP to calculate the average of the marks(points)
+list1=[8.0,7.0,8.0,8.0,9.0,8.0]
+sum1=0
+for i in list1:
+    sum1=sum1+i
+average=sum1/len(list1)
+print("Total CGPA of ssc:",average)
