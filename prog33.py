72cd520e11064a6038f988bd05ed288b2296e890
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-05 21:28:24 +0530

    Add files via upload
    playing pizz and buss game

---
+++ prog33.py
@@ -0,0 +1,10 @@
+#playing pizz and buss
+for i in range(1,101):
+    if(i%3==0):
+        print("the player says pizz",i)
+    elif(i%5==0):
+        print("the player says buss",i)
+    elif(i%==3 and i%==5):
+        print("the player says piss and buss",i)
+    else:
+        print("the player says number",i)
