3539a7024a806b9e2e34f5662e95d59c34fdde0a
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-16 22:01:10 +0530

    Add files via upload
    swapping the two values by using bitwise operators

---
+++ prog37.py
@@ -0,0 +1,10 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+print("value of a is:",a,"value of b is:",b)
+a=a^b
+b=a^b
+a=a^b
+print("value of a:",a)
+print("value of b:",b)
+
+
