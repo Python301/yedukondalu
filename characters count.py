d350680ab056ddfe96c9379b8f68a8426f6c43e2
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-19 22:42:26 +0530

    Add files via upload
    To count the characters in the string

---
+++ characters count.py
@@ -0,0 +1,7 @@
+string=input("enter the string:")
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+res=0
+for i in list:
+    res=string.count(i)
+    print(i,res)
+    
