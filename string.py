a9799d05b613b5e8abba069e9e1217eba5c12949
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-19 23:31:52 +0530

    Add files via upload
    To print the upper characters only

---
+++ string.py
@@ -0,0 +1,5 @@
+string=input("enter the string:")
+lower='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'
+for i in string:
+    if(i not in lower):
+        print(i)
