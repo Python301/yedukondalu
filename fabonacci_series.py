96f8886b0789ea127f19dd20789249e45f3fccc3
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-19 22:55:22 +0530

    Add files via upload
    fabonaci series

---
+++ fabonacci_series.py
@@ -0,0 +1,13 @@
+#fabonaci series
+n=int(input("enter the no of terms:"))
+f=0
+s=1
+if(n<=0):
+    print("the required series is",f)
+else:
+    print(f,s,end=" ")
+    for i in range(2,n):
+        next=f+s
+        print(next,end=" ")
+        f=s
+        s=next
