9fa9680cd4a2daf1393dc2918dc4b0b3604cb0c4
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-19 23:43:24 +0530

    Add files via upload
    Triangle patterns

---
+++ triangle.py
@@ -0,0 +1,167 @@
+n=int(input("enter how many rows u want:"))
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+
+print("*********** 2 *********")
+
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+print("********** 3 ******")
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+print("*********4*******")
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+print("***********5*********")
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+print("***********6**************")
+
+for i in range(n):
+    for j in range(i,n-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+print("*******************************")
+for i in range(n):
+    for j in range(i,n-1):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+print("***********7**************")
+
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+print("***********   8  **************")
+
+for i in range(n-1):
+    for j in range(i,n-1):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i,n-1):
+        print("*",end="")
+    for j in range(i,n):
+        print("*",end="")
+    print()
+print("*******************************")
+for i in range(n-1):
+    for j in range(i,n-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+
+
+print("*********** 9  *********")
+
+for i in range(n-1):
+    for j in range(i+1):
+        print("*",end="")
+    for j in range(i,n-1):
+        print(" ",end="")
+    for j in range(i,n-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+for i in range(n):
+    for j in range(i,n):
+        print("*",end="")
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i,n):
+        print("*",end="")
+    for j in range(i,n):
+        print("*",end="")
+    print()
+        
+
+print('*******************************')
+for i in range(n-1):
+    for j in range(i+1):
+        print("*",end="")
+    for j in range(i,n-1):
+        print(" ",end="")
+    for j in range(i,n-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+for i in range(n):
+    for j in range(i,n):
+        print("*",end="")
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i,n):
+        print("*",end="")
+    for j in range(i,n):
+        print("*",end="")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
