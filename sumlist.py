8e9c80f956c4da59d546c03f08749abe2c543d70
Python301 <108857090+Python301@users.noreply.github.com>
2022-08-19 23:36:18 +0530

    Add files via upload
    sum of the elements in the list

---
+++ sumlist.py
@@ -0,0 +1,13 @@
+#sum of the elements in the list
+n=int(input("enter how many elements u want:"))
+list=[]
+i=0
+while(i<n):
+    list.append(int(input()))
+    i=i+1
+print(list)
+list==list1
+total=0
+for j in range(0,len(list1)):
+    sum=total+list1(j)
+print("sum of list:",sum)
