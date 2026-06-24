39c3d023b3d1bbca0f27f29d986fb5fc4c669dc9
B.Yedukondalu <108857090+Python301@users.noreply.github.com>
2026-06-24 15:21:35 +0530

    Update fabonacci_series.py

--- fabonacci_series.py
+++ fabonacci_series.py
@@ -1,3 +1,18 @@
+# Writing FABONACCI Series!!!!!!!!!
+fib=[0,1]
+LenSeries = int(input("Enter lenght of series:"))
+
+if LenSeries <= 0:
+    print("Please enter a positive number.")
+elif LenSeries == 1:
+    print(0)
+else:
+    for _ in range(2,LenSeries):
+        fib.append(fib[-1]+fib[-2])
+    for i in fib:
+        print(i)
+
+
 #fabonaci series
 n=int(input("enter the no of terms:"))
 f=0
