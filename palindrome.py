b1a9221c9d03d1714bfc8ebd1c652c29e61a7eac
B.Yedukondalu <108857090+Python301@users.noreply.github.com>
2026-06-24 15:24:03 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,11 @@
-#Given string is palindrom or not
-name=input("enter the string:")
-string=name[::-1]
-if(name==string):
-    print("Given name is palindrom:",name)
+#Palindrome program
+
+str1 = input("Enter string: ")
+rstr1 = ""
+for i in range(len(str1)):
+    rstr1=str1[i]+rstr1
+print("Reverse of string: ", rstr1)
+if str1==rstr1:
+    print("Given string is palindrome")
 else:
-    print("Given name is not a palindrom:",name)
+    print("Given string is not palindrome")
